f4e4acc3e3d020625960969219c51798242391f0
CristianSusanu <cristian.augustin.susanu@gmail.com>
2022-01-18 12:02:19 +0200

    Changed Date Field Day

--- src/main/java/Day.java
+++ src/main/java/Day.java
@@ -1,9 +1,13 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Day {
     public ArrayList<Track> tracks;
+    private Date date;
 
     public Day(ArrayList<Track> tracks){
         this.tracks = tracks;
+        this.date = new Date();
     }
 }
